b4870759001882003c7469a1a75ee8e6132c691d
steppovver <61292996+steppovver@users.noreply.github.com>
2021-08-16 22:52:40 +0300

    add camera move by swiping

---
+++ LitePoly Project/Assets/CameraOrbit.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Transform Target;
+
+    public float distance = 20.0f;
+    public float xSpeed = 20.0f;
+    public float ySpeed = 20.0f;
+    public float yMinLimit = -90f;
+    public float yMaxLimit = 90f;
+    public float distanceMin = 10f;
+    public float distanceMax = 10f;
+    public float smoothTime = 2f;
+    float rotationYAxis = 0.0f;
+    float rotationXAxis = 0.0f;
+    float velocityX = 0.0f;
+    float velocityY = 0.0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            velocityX += xSpeed * Input.GetAxis("Mouse X") * distance * 0.02f;
+            velocityY += ySpeed * Input.GetAxis("Mouse Y") * 0.02f;
+            print(velocityX);
+            print(velocityY);
+        }
+        rotationYAxis += velocityX;
+        rotationXAxis -= velocityY;
+        rotationXAxis = ClampAngle(rotationXAxis, yMinLimit, yMaxLimit);
+
+        Quaternion rotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
+
+        Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+        Vector3 position = rotation * negDistance + Target.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
+        velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * smoothTime);
+        velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * smoothTime);
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
