58699b5f48fed08948ad7284027ef6f1c116ebc0
steppovver <61292996+steppovver@users.noreply.github.com>
2021-08-20 20:48:31 +0300

    Camera Movement like in Voard Kings

--- LitePoly Project/Assets/CameraOrbit.cs
+++ LitePoly Project/Assets/CameraOrbit.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class CameraOrbit : MonoBehaviour
 {
     public Transform Target;
+    public Transform Flor;
 
     public float distance = 20.0f;
     public float xSpeed = 20.0f;
@@ -20,12 +21,19 @@ public class CameraOrbit : MonoBehaviour
     float rotationXAxis = 0.0f;
     float velocityX = 0.0f;
     float velocityY = 0.0f;
+    float velocityXMove = 0.0f;
+    float velocityYMove = 0.0f;
     float velocityZoom = 0.0f;
 
-    Vector3 position;
+    Vector3 _position;
+    Vector3 _sizeOfField;
 
     private void Start()
     {
+        Target.position = Flor.position;
+
+        GetSizeOfFieled();
+
         rotationXAxis = transform.rotation.eulerAngles.x;
         rotationYAxis = transform.rotation.eulerAngles.y;
 
@@ -35,29 +43,36 @@ public class CameraOrbit : MonoBehaviour
 
     void Update()
     {
-
-            OneTouchInput();
-            TwoTouchInput();
-
-        if (Application.platform != RuntimePlatform.Android)
-        {
-            DragMouseOrbit();
-
-        }
-
+        TwoTouchInput();
+        OneTouchInput();
 
         Target.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+    }
+
+    void GetSizeOfFieled()
+    {
+        _sizeOfField = Flor.GetComponent<MeshRenderer>().bounds.size;
+
+        print(_sizeOfField);
     }
 
     void OneTouchInput()
     {
         if (Input.touchCount == 1)
         {
-            velocityX += xSpeed * Input.GetTouch(0).deltaPosition.x * 0.02f;
-            velocityY += ySpeed * Input.GetTouch(0).deltaPosition.y * 0.02f / 10;
+            velocityXMove += Input.GetTouch(0).deltaPosition.x * 0.001f;
+            velocityYMove += Input.GetTouch(0).deltaPosition.y * 0.001f;
         }
 
-        CalculateRotation();
+        Target.Translate(-velocityXMove, 0, -velocityYMove, Space.Self);
+
+        float newXPos = Mathf.Clamp(Target.position.x, Flor.position.x - _sizeOfField.x/2, Flor.position.x + _sizeOfField.x/2);
+        float newZPos = Mathf.Clamp(Target.position.z, Flor.position.z - _sizeOfField.z/2, Flor.position.z + _sizeOfField.z/2);
+
+        Target.position = new Vector3(newXPos, Target.position.y, newZPos);
+
+        velocityXMove = Mathf.Lerp(velocityXMove, 0, Time.deltaTime * smoothTime);
+        velocityYMove = Mathf.Lerp(velocityYMove, 0, Time.deltaTime * smoothTime);
     }
 
     void TwoTouchInput()
@@ -69,11 +84,26 @@ public class CameraOrbit : MonoBehaviour
             float touchDelta = curDist.magnitude - prevDist.magnitude;
             velocityZoom += zoomSpeed * touchDelta * 0.005f;
 
-            distance = Mathf.Clamp(distance - velocityZoom * Time.deltaTime, 5f, 20f);
-            distance = Mathf.Clamp(distance - velocityZoom * Time.deltaTime, 5f, 20f);
 
-            velocityZoom = Mathf.Lerp(velocityZoom, 0, Time.deltaTime * smoothTime);
+            if (touchDelta < 1000)
+            {
+
+                velocityX += xSpeed * Input.GetTouch(0).deltaPosition.x * 0.01f;
+                velocityY += ySpeed * Input.GetTouch(0).deltaPosition.y * 0.01f / 10;
+            }
+            else
+            {
+                velocityZoom += zoomSpeed * touchDelta * 0.005f;
+            }
         }
+
+        CalculateRotation();
+
+        distance = Mathf.Clamp(distance - velocityZoom * Time.deltaTime, 5f, 20f);
+        distance = Mathf.Clamp(distance - velocityZoom * Time.deltaTime, 5f, 20f);
+
+
+        velocityZoom = Mathf.Lerp(velocityZoom, 0, Time.deltaTime * smoothTime);
     }
 
     void DragMouseOrbit()
@@ -105,10 +135,10 @@ public class CameraOrbit : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(rotationXAxis, rotationYAxis, 0);
 
         Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
-        position = rotation * negDistance + Target.position;
+        _position = rotation * negDistance + Target.position;
 
         transform.rotation = rotation;
-        transform.position = position;
+        transform.position = _position;
         velocityX = Mathf.Lerp(velocityX, 0, Time.deltaTime * smoothTime);
         velocityY = Mathf.Lerp(velocityY, 0, Time.deltaTime * smoothTime);
     }
